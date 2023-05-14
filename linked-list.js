38eaf14044247c07117b6ada767c24e829b22f0a
Shahabr99 <sean.rohani9@gmail.com>
2023-05-14 13:23:50 -0700

    removeAt

--- linked-list.js
+++ linked-list.js
@@ -45,31 +45,72 @@ class LinkedList {
   /** pop(): return & remove last item. */
 
   pop() {
-    if (!this.head) return null;
-    const prevLastNode = this.tail;
-    const currLastNode = prevLastNode.prev;
-    this.tail = currLastNode;
-    currLastNode.next = null;
-    return prevLastNode.val;
+    try {
+      const prevLastNode = this.tail;
+      const currLastNode = prevLastNode.prev;
+      this.tail = currLastNode;
+      currLastNode.next = null;
+      return prevLastNode.val;
+    } catch (error) {
+      return null;
+    }
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
-    if (!this.head) return null;
+    if (!this.head) throw new Error("List is empty");
+    const prevHead = this.head;
+    this.head = prevHead.next;
+    return prevHead.val;
   }
 
   /** getAt(idx): get val at idx. */
 
-  getAt(idx) {}
+  getAt(idx) {
+    let currIndex = 0;
+    let currNode = this.head;
+    while (currNode) {
+      if (currIndex === idx) return currNode.val;
+      currIndex++;
+      currNode = currNode.next;
+    }
+  }
 
   /** setAt(idx, val): set val at idx to val */
 
-  setAt(idx, val) {}
+  setAt(idx, val) {
+    let currIndex = 0;
+    let currNode = this.head;
+    while (currNode) {
+      if (currIndex === idx) {
+        currNode.val = val;
+        return currNode;
+      }
+      currIndex++;
+      currNode = currNode.next;
+    }
+    throw new Error("invalid index");
+  }
 
   /** insertAt(idx, val): add node w/val before idx. */
 
-  insertAt(idx, val) {}
+  insertAt(idx, val) {
+    let currNode = this.head;
+    let newNode = { val };
+
+    let currIndex = 0;
+    while (currNode) {
+      if (currIndex === idx - 1) {
+        newNode.next = currNode.next;
+        currNode.next = newNode;
+      } else {
+        currNode = currNode.next;
+        currIndex++;
+      }
+    }
+    throw new Error("Invalid Index");
+  }
 
   /** removeAt(idx): return & remove item at idx, */
 
